47059b243a7282543cc938bf6fd8ae0d51a0c2fb
zli34 <zli34@ncsu.edu>
2018-04-09 00:05:19 +0800

    add criminal's entering rate lambda

--- config/environ_config.py
+++ config/environ_config.py
@@ -11,13 +11,14 @@ environ = {
     'resources_init_max_for_criminal': 25,
     'resources_init_max_for_civilian': 100,
     'crime_propensity_init_max': 25,
-    'crime_distance':     1,
+    'crime_distance':     0,
     'agent_vision_limit': 1,   # Number of cells an agent can see around itself in a grid
     'grid_width':         50,  # Width of grid in number of cells
     'grid_height':        50,  # Height of grid in number of cells
-    'crime_propensity_threshold': 0,  # The propensity required to commit crimes or be a solo criminal
+    'crime_propensity_threshold': 20,  # The propensity required to commit crimes or be a solo criminal
     'police_dispatch': 'closest',  # 'random'/'closest' behavior for dispatching police to crime scenes
-    'civilian_vision_radius': 3,
+    'civilian_vision_radius': 1,
     'police_vision_radius': 1,
-    'police_arrest_probability': 0.2
+    'police_arrest_probability': 0.2,
+    'lambda': 0.05 # mean of criminals entering the grid per second
 }
