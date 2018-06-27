83319d30b592d47042f9bfcc2571416ae485a665
cma43 <cmartman@ncsu.edu>
2018-06-27 12:52:09 -0400

    Small changes to config

--- Examples/BWT/config/environ_config.py
+++ Examples/BWT/config/environ_config.py
@@ -27,11 +27,7 @@ environ = {
     'maximum_sentence': 20,  # if arrest_behavior is imprison, the max length of time to imprison a criminal
     'coalition_merge_distance': 0,
     'crime_success_probability': 0.8,
-    'alpha': .5,   #The output elasticity for agent's utility functions/the % change in output per 1% change in all inputs
     'gamma': 1, #Parameter for how agents substitute between inputs using constant elasticity of substitution (CES) utility function;
-    'utility_function_type': 'type_2', #Preset utility functions: Type 1 := CES w/ gamma=1 (perfect substitution);
-                                      #                          Type 2 := CES w/gamma=negative infinity (No substitution);
-                                      #                          Type 3 := CES w/gamma=0 (Unit elasticity of subsitution)
     'travel_penalty': 2,  #The penalty associated with a criminal traveling farther from their home base, or a civilian into dangerous areas
     'kappa': .8,         # discount parameter for computing total utility: 1 means perfectly future oriented and 0 means perfectly present oriented      
     'police_arrest_radius': 1,
